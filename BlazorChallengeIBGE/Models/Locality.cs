4703e86fd7e20a06a1470dea018422446451bd46
Thiago Cajaiba <thiago.cajaiba@gmail.com>
2023-12-21 08:59:10 -0300

    rev ErrorMessage Regex

--- BlazorChallengeIBGE/Models/Locality.cs
+++ BlazorChallengeIBGE/Models/Locality.cs
@@ -9,18 +9,17 @@ namespace BlazorChallengeIBGE.Models
 
     [Required(ErrorMessage = "Informe o código IBGE")]
     [StringLength(7, MinimumLength = 7, ErrorMessage = "O código deve conter 7 digitos")]
-    [RegularExpression(@"^\d+$")] // Somente dígitos
+    [RegularExpression(@"^\d+$", ErrorMessage = "Apenas digitos são permitidos")]
     public string IbgeCode { get; set; } = null!;
 
     [Required(ErrorMessage = "Informe a sigla do Estado")]
     [StringLength(2, MinimumLength = 2, ErrorMessage = "A sigla deve conter 2 caracteres")]
-    [RegularExpression(@"^[a-zA-Z]*$")] // Somente letras
+    [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Apenas letras são permitidas")]
     public string State { get; set; } = null!;
 
     [Required(ErrorMessage = "Informe a Cidade")]
     [MinLength(3, ErrorMessage = "A cidade deve ter pelo menos 3 caracteres")]
     [MaxLength(100, ErrorMessage = "A cidade deve ter no máximo 100 caracteres")]
-    [RegularExpression(@"^[a-zA-ZÀ-ÿ ]*$")] // Somente letras e acentuação pt-BR
-    public string City { get; set; } = null!;
+    [RegularExpression(@"^[a-zA-ZÀ-ÿ ]*$", ErrorMessage = "Apenas letras e acentuação são permitidos")]
   }
 }
